7014dabd68c7b8a8e1eaf72fab7970510618f872
Jacek Góraj <jckgrj@gmail.com>
2018-06-26 21:04:50 +0200

    moved log texts to json file

--- proj/src/logs/logs.js
+++ proj/src/logs/logs.js
@@ -1,25 +1,17 @@
 var protractor = require('protractor');
 var fs = require('fs');
+var logsTextValues = require('./logs-text-values.json');
 let currentPage = 'not specified yet';
 let allPagesList = [];
 const currentTime = new Date();
 let isNewRun = true;
-
+/*
 const getCurrentDate = () => {
     return `${currentTime.getFullYear()}-${currentTime.getMonth() + 1}-${currentTime.getDate()}-${currentTime.getHours()}-${currentTime.getMinutes()}-${currentTime.getSeconds()}-${currentTime.getMilliseconds()}`;
 };
 
-let currentDate = getCurrentDate();
+let currentDate = getCurrentDate();*/
 const PATH = './logs';
-
-const ELEMENT_CLEAR_LOG = 'element.clear()';
-const ELEMENT_CLICK_LOG = 'element.click()';
-const ELEMENT_GET_ATTRIBUTE_LOG = 'element.getAttribute()';
-const ELEMENT_GET_TEXT_LOG = 'element.getText()';
-const ELEMENT_IS_DISPLAYED_LOG = 'element.isDisplayed()';
-const ELEMENT_IS_PRESENT_LOG = 'element.isPresent()';
-const ELEMENT_SEND_KEYS_LOG = 'element.sendKeys()';
-const URL_OPENED = 'browser.get()';
 
 class ElementFinder {
     constructor(handler){
@@ -35,28 +27,28 @@ class ElementFinder {
 
     clear() {
         this.element.getLocation().then(location => {
-            addLogs(ELEMENT_CLEAR_LOG, null, location.x, location.y);
+            addLogs(logsTextValues.ELEMENT_CLEAR_LOG, null, location.x, location.y);
         });
         return this.element.clear();
     };
 
     click() {
         this.element.getLocation().then(location => {
-            addLogs(ELEMENT_CLICK_LOG, null, location.x, location.y);
+            addLogs(logsTextValues.ELEMENT_CLICK_LOG, null, location.x, location.y);
         });
         return this.element.click();
     };
 
     getAttribute(attribute) {
         this.element.getLocation().then(location => {
-            addLogs(ELEMENT_GET_ATTRIBUTE_LOG, attribute, location.x, location.y);
+            addLogs(logsTextValues.ELEMENT_GET_ATTRIBUTE_LOG, attribute, location.x, location.y);
         });
         return this.element.getAttribute(attribute);
     };
 
     getText() {
         this.element.getLocation().then(location => {
-            addLogs(ELEMENT_GET_TEXT_LOG, null, location.x, location.y);
+            addLogs(logsTextValues.ELEMENT_GET_TEXT_LOG, null, location.x, location.y);
         });
         return this.element.getText();
     };
@@ -66,10 +58,10 @@ class ElementFinder {
             this.element.isPresent().then(isPresent => {
                 if (isPresent) {
                     this.element.isDisplayed().then(isDisplayed => {
-                        addLogs(ELEMENT_IS_DISPLAYED_LOG, isDisplayed, location.x, location.y);
+                        addLogs(logsTextValues.ELEMENT_IS_DISPLAYED_LOG, isDisplayed, location.x, location.y);
                     });
                 } else {
-                    addLogs(ELEMENT_IS_DISPLAYED_LOG, isPresent, location.x, location.y);
+                    addLogs(logsTextValues.ELEMENT_IS_DISPLAYED_LOG, isPresent, location.x, location.y);
                 }
             });
         });
@@ -80,9 +72,9 @@ class ElementFinder {
         this.element.getLocation().then(location => {
             this.element.isPresent().then(isPresent => {
                 if (isPresent) {
-                        addLogs(ELEMENT_IS_PRESENT_LOG, isPresent, location.x, location.y);
+                        addLogs(logsTextValues.ELEMENT_IS_PRESENT_LOG, isPresent, location.x, location.y);
                 } else {
-                    addLogs(ELEMENT_IS_PRESENT_LOG, isPresent, location.x, location.y);
+                    addLogs(logsTextValues.ELEMENT_IS_PRESENT_LOG, isPresent, location.x, location.y);
                 }
             });
         });
@@ -91,7 +83,7 @@ class ElementFinder {
 
     sendKeys(query) {
         this.element.getLocation().then(location => {
-            addLogs(ELEMENT_SEND_KEYS_LOG, query, location.x, location.y);
+            addLogs(logsTextValues.ELEMENT_SEND_KEYS_LOG, query, location.x, location.y);
         });
         return this.element.sendKeys(query);
     };
@@ -134,7 +126,7 @@ const addLogs = (action, value, x, y, page = currentPage) => {
 };
 
 const openUrl = (url) => {
-    addLogs(URL_OPENED, url, null, null);
+    addLogs(logsTextValues.URL_OPENED_LOG, url, null, null);
     return protractor.browser.get(url);
 };
 
