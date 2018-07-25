e63c3db6c0ead2135da35e790343b85e2435836c
Jacek Góraj <jckgrj@gmail.com>
2018-07-25 20:39:58 +0200

    alert pops up on click to the body!

--- proj/dist/index.js
+++ proj/dist/index.js
@@ -3,14 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // creating a "var module: any" will allow use of module.exports
 var myPlugin = {
     onPageLoad: function (browser) {
-        setHTMLValue(browser);
+        browser.executeScript('$("body").on("click", () => { alert("OOOOPS") })');
     }
 };
-function setHTMLValue(browser) {
-    // function setInnerHTML (div, content) {
-    //     div.innerHTML = content;
-    // }
-    browser.executeScript('$("body").attr("style", "background-color: red")');
-}
 module.exports = myPlugin;
 //# sourceMappingURL=index.js.map
--- proj/src/index.ts
+++ proj/src/index.ts
@@ -5,17 +5,10 @@ declare var module: any;
 // creating a "var module: any" will allow use of module.exports
 
 let myPlugin: ProtractorPlugin = {
-onPageLoad(browser: ProtractorBrowser) {
-
-    setHTMLValue(browser);
-  }
+    onPageLoad(browser: ProtractorBrowser) {
+        browser.executeScript('$("body").on("click", () => { alert("OOOOPS") })');
+    }
 };
 
-function setHTMLValue(browser: ProtractorBrowser) {
-    // function setInnerHTML (div, content) {
-    //     div.innerHTML = content;
-    // }
-    browser.executeScript('$("body").attr("style", "background-color: red")');
-}
 
 module.exports = myPlugin;
